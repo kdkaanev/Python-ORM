3241016d46afcf4a6d11769bda123839a6e34b25
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-10-21 11:48:39 +0300

    up

--- Django Models Basics/Exercises/01. Person_Skeleton/main_app/models.py
+++ Django Models Basics/Exercises/01. Person_Skeleton/main_app/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
-
 # Create your models here.
+CHOICES = [
+    ('fic', "Fiction"),
+    ('non_fic', "Non-Fiction"),
+    ('sci_fic', "Science Fiction"),
+    ('hor', "Horror")
+]
+
+
 class Person(models.Model):
     name = models.CharField(max_length=30)
     age = models.PositiveIntegerField()
@@ -34,3 +41,36 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 
+class UserProfile(models.Model):
+    username = models.CharField(max_length=65, unique=True)
+    first_name = models.CharField(max_length=40, unique=True)
+    last_name = models.CharField(max_length=40, unique=True)
+    email = models.EmailField(unique=True, default="students@softuni.bg")
+    bio = models.TextField(max_length=120)
+    profile_image_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    difficulty_level = models.CharField(max_length=20)
+    duration_minutes = models.PositiveIntegerField()
+    equipment = models.CharField(max_length=90)
+    video_url = models.URLField(blank=True)
+    calories_burned = models.PositiveIntegerField(default=0)
+    is_favorite = models.BooleanField(default=False)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=30)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=20, choices=CHOICES)
+    publication_date = models.DateField(editable=False, auto_now_add=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    rating = models.FloatField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
