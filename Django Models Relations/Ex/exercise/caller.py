73981ff3cbd5fca923214be053d4ef326d50de49
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-11-06 07:28:48 +0200

    task2

--- Django Models Relations/Ex/exercise/caller.py
+++ Django Models Relations/Ex/exercise/caller.py
@@ -1,28 +1,44 @@
 import os
 import django
+from django.db.models import QuerySet
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-from main_app.models import Author, Book
+from main_app.models import Artist, Song
 
 
-# Create queries within functions
-def show_all_authors_with_their_books() -> str:
-    authors_with_books = []
-    authors = Author.objects.all().order_by('id')
-    for author in authors:
-        books = Book.objects.filter(author=author)
-        if not books:
-            continue
+# from main_app.models import Author, Book
+#
+#
+# # Create queries within functions
+# def show_all_authors_with_their_books() -> str:
+#     authors_with_books = []
+#     authors = Author.objects.all().order_by('id')
+#     for author in authors:
+#         books = Book.objects.filter(author=author)
+#         if not books:
+#             continue
+#
+#         titles = ', '.join([book.title for book in books])
+#         authors_with_books.append(f'{author.name} has written - {titles}!')
+#
+#     return '\n'.join(authors_with_books)
+#
+#
+# def delete_all_authors_without_books() -> None:
+#     authors_without_books = Author.objects.filter(book__isnull=True).delete()
 
-        titles = ', '.join([book.title for book in books])
-        authors_with_books.append(f'{author.name} has written - {titles}!')
 
-    return '\n'.join(authors_with_books)
+def add_song_to_artist(artist_name: str, song_title: str):
+    artist = Artist.objects.get(name=artist_name)
+    song = Song.objects.get(title=song_title)
+
+    artist.songs.add(song)
 
 
-def delete_all_authors_without_books() -> None:
-    authors_without_books = Author.objects.filter(book__isnull=True).delete()
+def get_songs_by_artist(artist_name: str) -> QuerySet[Artist]:
+    artist = Artist.objects.get(name=artist_name)
+    return artist.songs.all().order_by('-id')
--- Django Models Relations/Ex/exercise/main_app/models.py
+++ Django Models Relations/Ex/exercise/main_app/models.py
@@ -13,3 +13,22 @@ class Book(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     author = models.ForeignKey(to=Author, on_delete=models.CASCADE)
 
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    songs = models.ManyToManyField(Song, related_name='artists')
+
+
+
+
+
+
+
+
+
+
+
