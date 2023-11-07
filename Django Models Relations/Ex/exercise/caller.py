d29ec97fe78a70cef689924196ada753250f6e3f
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-11-07 05:37:52 +0200

    task2

--- Django Models Relations/Ex/exercise/caller.py
+++ Django Models Relations/Ex/exercise/caller.py
@@ -42,3 +42,46 @@ def add_song_to_artist(artist_name: str, song_title: str):
 def get_songs_by_artist(artist_name: str) -> QuerySet[Artist]:
     artist = Artist.objects.get(name=artist_name)
     return artist.songs.all().order_by('-id')
+
+
+def remove_song_from_artist(artist_name: str, song_title: str):
+
+    artist = Artist.objects.get(name=artist_name)
+    song = Song.objects.get(title=song_title)
+
+    artist.songs.remove(song)
+
+    # Create artists
+
+
+artist1 = Artist.objects.create(name="Daniel Di Angelo")
+artist2 = Artist.objects.create(name="Indila")
+
+# Create songs
+song1 = Song.objects.create(title="Lose Face")
+song2 = Song.objects.create(title="Tourner Dans Le Vide")
+song3 = Song.objects.create(title="Loyalty")
+
+# Add a song to an artist
+add_song_to_artist("Daniel Di Angelo", "Lose Face")
+add_song_to_artist("Daniel Di Angelo", "Loyalty")
+add_song_to_artist("Indila", "Tourner Dans Le Vide")
+
+# Get all songs by a specific artist
+songs = get_songs_by_artist("Daniel Di Angelo")
+for song in songs:
+    print(f"Daniel Di Angelo: {song.title}")
+
+# Get all songs by a specific artist
+songs = get_songs_by_artist("Indila")
+for song in songs:
+    print(f"Indila: {song.title}")
+
+# Remove a song from an artist
+remove_song_from_artist("Daniel Di Angelo", "Lose Face")
+
+# Check if the song is removed
+songs = get_songs_by_artist("Daniel Di Angelo")
+
+for song in songs:
+    print(f"Songs by Daniel Di Angelo after removal: {song.title}")
