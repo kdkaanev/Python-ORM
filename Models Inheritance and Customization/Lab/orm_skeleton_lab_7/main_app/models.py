8434403a23fcd7d79507f13fcbea65f2c461f723
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-11-11 19:48:35 +0200

    task2

--- Models Inheritance and Customization/Lab/orm_skeleton_lab_7/main_app/models.py
+++ Models Inheritance and Customization/Lab/orm_skeleton_lab_7/main_app/models.py
@@ -12,9 +12,35 @@ class Animal(models.Model):
 class Mammal(Animal):
     fur_color = models.CharField(max_length=50)
 
+
 class Bird(Animal):
     wing_span = models.DecimalField(max_digits=5, decimal_places=2)
 
 
 class Reptile(Animal):
     scale_type = models.CharField(max_length=50)
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=10)
+
+    class Meta:
+        abstract = True
+
+
+class ZooKeeper(Employee):
+    class Speciality(models.TextChoices):
+        Mammals = 'Mammals'
+        Birds = 'Birds'
+        Reptiles = 'Reptiles'
+        Others = 'Others'
+
+    specialty = models.CharField(max_length=10, choices=Speciality.choices)
+
+    managed_animals = models.ManyToManyField(to=Animal)
+
+
+class Veterinarian(Employee):
+    license_number = models.CharField(max_length=10)
