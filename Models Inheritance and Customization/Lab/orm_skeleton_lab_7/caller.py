b1094ae6c81316be25b7c72144fdf8907ea3a292
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-11-13 17:55:30 +0200

    final up for lab7

--- Models Inheritance and Customization/Lab/orm_skeleton_lab_7/caller.py
+++ Models Inheritance and Customization/Lab/orm_skeleton_lab_7/caller.py
@@ -5,6 +5,7 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
+
 # Import your models
 # Create and check models
 # Run and print your queries
