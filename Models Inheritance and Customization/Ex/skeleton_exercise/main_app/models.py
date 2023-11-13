e05382b388fc3927481db7fbc18d9696c269f46d
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-11-13 21:04:25 +0200

    up

--- Models Inheritance and Customization/Ex/skeleton_exercise/main_app/models.py
+++ Models Inheritance and Customization/Ex/skeleton_exercise/main_app/models.py
@@ -50,3 +50,34 @@ class VengeanceDemonHunter(DemonHunter):
 
 class FelbladeDemonHunter(DemonHunter):
     felblade_ability = models.CharField(max_length=100)
+
+
+class UserProfile(models.Model):
+    username = models.CharField(max_length=70, unique=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(null=True, blank=True)
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(to=UserProfile, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(to=UserProfile, related_name='received_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    is_read = models.BooleanField(default=False)
+    
+    @property
+    def mark_as_read(self):
+        self.is_read = True
+        return self.is_read
+    
+    @property
+    def mark_as_unread(self):
+        self.is_read = False
+        return self.is_read
+
+    def reply_to_message(self, reply_content, receiver):
+       pass
+
+
+    
+
+
