7a67e8751f7bc8506157012a52f6e138d51731c3
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-11-13 12:49:03 +0200

    up task5

--- Models Inheritance and Customization/Lab/orm_skeleton_lab_7/caller.py
+++ Models Inheritance and Customization/Lab/orm_skeleton_lab_7/caller.py
@@ -8,11 +8,13 @@ django.setup()
 # Import your models
 # Create and check models
 # Run and print your queries
-from main_app.models import ZooDisplayAnimal
+from datetime import date, timedelta
+from main_app.models import Mammal, Reptile
 
-is_proxy = ZooDisplayAnimal._meta.proxy
+lion_birth_date = date.today() - timedelta(days=731)
+lion = Mammal.objects.create(name="Simba", species="Lion", birth_date=lion_birth_date, sound="Roar", fur_color="Golden")
+print(f"The lion's age is {lion.age}.")
 
-if is_proxy:
-    print("ZooDisplayAnimal is a proxy model.")
-else:
-    print("ZooDisplayAnimal is not a proxy model.")
+snake_birth_date = date.today() - timedelta(days=30)
+snake = Reptile.objects.create(name="Kaa", species="Python", birth_date=snake_birth_date, sound="Hiss", scale_type="Scales")
+print(f"The snake's age is {snake.age}.")
--- Models Inheritance and Customization/Lab/orm_skeleton_lab_7/main_app/models.py
+++ Models Inheritance and Customization/Lab/orm_skeleton_lab_7/main_app/models.py
@@ -1,5 +1,6 @@
+from django.core.exceptions import ValidationError
 from django.db import models
-
+from datetime import date
 
 # Create your models here.
 class Animal(models.Model):
@@ -7,6 +8,14 @@ class Animal(models.Model):
     species = models.CharField(max_length=100)
     birth_date = models.DateField()
     sound = models.CharField(max_length=100)
+    
+    @property
+    def age(self):
+        today = date.today()
+        age = today.year - self.birth_date.year - (
+                (today.month, today.day) <
+                (self.birth_date.month, self.birth_date.day))
+        return age
 
 
 class Mammal(Animal):
@@ -41,6 +50,12 @@ class ZooKeeper(Employee):
 
     managed_animals = models.ManyToManyField(to=Animal)
 
+    def clean(self):
+        super().clean()
+
+        if self.specialty not in self.Speciality:
+            raise ValidationError("Specialty must be a valid choice.")
+
 
 class Veterinarian(Employee):
     license_number = models.CharField(max_length=10)
@@ -49,3 +64,20 @@ class Veterinarian(Employee):
 class ZooDisplayAnimal(Animal):
     class Meta:
         proxy = True
+
+    def display_info(self):
+        return f"Meet {self.name}! It's {self.species} and it's born {self.birth_date}. It makes a noise like '{self.sound}'!{self.__extra_info()}"
+
+    def __extra_info(self):
+        info = ''
+        if hasattr(self, 'mammal'):
+            info = f"Its fur color is {self.mammal.fur_color}."
+        elif hasattr(self, 'bird'):
+            info = f"Its wingspan is {self.bird.wing_span} cm."
+        elif hasattr(self, 'reptile'):
+            info = f"Its scale type is {self.reptile.scale_type}."
+        
+        return info
+
+    def is_endangered(self):
+        return True if self.species in ["Cross River Gorilla", "Orangutan", "Green Turtle"] else False
