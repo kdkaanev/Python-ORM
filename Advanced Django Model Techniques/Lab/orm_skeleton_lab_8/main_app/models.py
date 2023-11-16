87a4c2f9747017f3f73e774a9c8fcd59949a475c
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-11-16 07:16:59 +0200

    task5

--- Advanced Django Model Techniques/Lab/orm_skeleton_lab_8/main_app/models.py
+++ Advanced Django Model Techniques/Lab/orm_skeleton_lab_8/main_app/models.py
@@ -8,7 +8,6 @@ from main_app.validators import validate_menu_categories
 # Create your models here.
 
 
-
 class Restaurant(models.Model):
     name = models.CharField(
         max_length=100,
@@ -47,7 +46,7 @@ class Menu(models.Model):
     )
     restaurant = models.ForeignKey(
         to=Restaurant,
-        on_delete= models.CASCADE,
+        on_delete=models.CASCADE,
     )
 
 
@@ -75,8 +74,10 @@ class RestaurantReview(models.Model):
 
 
 class RegularRestaurantReview(RestaurantReview):
-   class Meta(RestaurantReview.Meta):
-       pass
+    class Meta(RestaurantReview.Meta):
+        pass
+
+
 class FoodCriticRestaurantReview(RestaurantReview):
     food_critic_cuisine_area = models.CharField(
         max_length=100,
@@ -87,32 +88,29 @@ class FoodCriticRestaurantReview(RestaurantReview):
         verbose_name_plural = 'Food Critic Reviews'
 
 
+class MenuReview(models.Model):
+    reviewer_name = models.CharField(
+        max_length=100,
+    )
+    menu = models.ForeignKey(
+        to=Menu,
+        on_delete=models.CASCADE,
+    )
+    review_content = models.TextField()
+    rating = models.PositiveIntegerField(
+        validators=[
+            validators.MaxValueValidator(5),
+        ],
+    )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    class Meta:
+        ordering = ['-rating']
+        verbose_name = 'Menu Review'
+        verbose_name_plural = 'Menu Reviews'
+        unique_together = ['reviewer_name', 'menu']
+        indexes=[
+            models.Index(
+                fields=['menu'],
+                name='main_app_menu_review_menu_id'
+            )
+        ]
