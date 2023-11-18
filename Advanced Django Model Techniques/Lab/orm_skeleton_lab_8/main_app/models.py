f3ea9d169810540b1db1a23ddec9e241bd3fd2d6
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-11-18 08:49:29 +0200

    task6

--- Advanced Django Model Techniques/Lab/orm_skeleton_lab_8/main_app/models.py
+++ Advanced Django Model Techniques/Lab/orm_skeleton_lab_8/main_app/models.py
@@ -6,6 +6,18 @@ from main_app.validators import validate_menu_categories
 
 
 # Create your models here.
+class ReviewMixin(models.Model):
+    rating = models.PositiveIntegerField(
+        validators=[
+            validators.MaxValueValidator(5),
+        ],
+    )
+    review_content = models.TextField()
+
+    class Meta:
+        abstract = True
+
+    ordering = ['-rating']
 
 
 class Restaurant(models.Model):
@@ -50,7 +62,7 @@ class Menu(models.Model):
     )
 
 
-class RestaurantReview(models.Model):
+class RestaurantReview(ReviewMixin):
     reviewer_name = models.CharField(
         max_length=100,
     )
@@ -58,16 +70,10 @@ class RestaurantReview(models.Model):
         to=Restaurant,
         on_delete=models.CASCADE,
     )
-    review_content = models.TextField()
-    rating = models.PositiveIntegerField(
-        validators=[
-            validators.MaxValueValidator(5),
-        ],
-    )
+
 
     class Meta:
         abstract = True
-        ordering = ['-rating']
         verbose_name = 'Restaurant Review'
         verbose_name_plural = 'Restaurant Reviews'
         unique_together = ['reviewer_name', 'restaurant']
@@ -88,7 +94,7 @@ class FoodCriticRestaurantReview(RestaurantReview):
         verbose_name_plural = 'Food Critic Reviews'
 
 
-class MenuReview(models.Model):
+class MenuReview(ReviewMixin):
     reviewer_name = models.CharField(
         max_length=100,
     )
@@ -96,15 +102,10 @@ class MenuReview(models.Model):
         to=Menu,
         on_delete=models.CASCADE,
     )
-    review_content = models.TextField()
-    rating = models.PositiveIntegerField(
-        validators=[
-            validators.MaxValueValidator(5),
-        ],
-    )
+
+
 
     class Meta:
-        ordering = ['-rating']
         verbose_name = 'Menu Review'
         verbose_name_plural = 'Menu Reviews'
         unique_together = ['reviewer_name', 'menu']
