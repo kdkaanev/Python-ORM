7a19b717f5d83c74f6503b650a955f77477b8fb5
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-11-19 11:39:26 +0200

    task3

--- Advanced Django Model Techniques/Ex/skeleton_exercise/main_app/models.py
+++ Advanced Django Model Techniques/Ex/skeleton_exercise/main_app/models.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 
 from django.db import models
 from django.core import validators
@@ -45,6 +46,7 @@ class BaseMedia(models.Model):
         abstract = True
         ordering = ['-created_at', 'title']
 
+
 class Book(BaseMedia):
     author = models.CharField(
         max_length=100,
@@ -74,10 +76,10 @@ class Movie(BaseMedia):
         ]
     )
 
-
     class Meta(BaseMedia.Meta):
         verbose_name = 'Model Movie'
         verbose_name_plural = 'Models of type - Movie'
+
 
 class Music(BaseMedia):
     artist = models.CharField(
@@ -92,15 +94,40 @@ class Music(BaseMedia):
         verbose_name_plural = 'Models of type - Music'
 
 
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    def calculate_tax(self) -> Decimal:
+        return self.price * Decimal(0.08)
+
+    @staticmethod
+    def calculate_shipping_cost(weight: Decimal) -> Decimal:
+        return weight * Decimal(2)
+
+    def format_product_name(self) -> str:
+        return f"Product: {self.name}"
 
 
+class DiscountedProduct(Product):
+    class Meta:
+        proxy = True
+
+    def calculate_price_without_discount(self) -> Decimal:
+        return self.price * Decimal(0.8)
+
+    def calculate_tax(self) -> Decimal:
+        return self.price * Decimal(0.05)
 
 
+    @staticmethod
+    def calculate_shipping_cost(weight: Decimal) -> Decimal:
+        return weight * Decimal(1.5)
 
-
-
-
-
-
-
-
+    def format_product_name(self) -> str:
+        return f'Discounted Product: {self.name}'
