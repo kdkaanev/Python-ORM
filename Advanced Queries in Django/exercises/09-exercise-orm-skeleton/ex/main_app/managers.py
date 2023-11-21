791a767df4b824fab2b3fc1be85c8691f02494ec
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-11-21 19:30:43 +0200

    ex9 task1

---
+++ Advanced Queries in Django/exercises/09-exercise-orm-skeleton/ex/main_app/managers.py
@@ -0,0 +1,18 @@
+from decimal import Decimal
+
+from django.db import models
+from django.db.models import QuerySet
+
+
+class RealEstateListingManager(models.Manager):
+    def by_property_type(self,property_type: str) -> QuerySet:
+        return self.filter(property_type=property_type)
+
+    def in_price_range(self,min_price: Decimal, max_price: Decimal) -> QuerySet:
+        return self.filter(price__range=(min_price,max_price))
+
+    def with_bedrooms(self,bedrooms_count: int) ->QuerySet:
+        return self.filter(bedrooms=bedrooms_count)
+
+    def popular_locations(self):
+        return self.values('location').annotate(location_count=models.Count('location')).order_by('id')[:2]
