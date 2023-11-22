c36911e8ce405abc43999cb7d619ca48e8a0572e
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-11-22 07:03:55 +0200

    task6

--- Advanced Queries in Django/exercises/09-exercise-orm-skeleton/ex/main_app/models.py
+++ Advanced Queries in Django/exercises/09-exercise-orm-skeleton/ex/main_app/models.py
@@ -132,9 +132,10 @@ class Task(models.Model):
         return cls.objects.filter(query)
 
     @classmethod
-    def search_tasks(cls,query: str) -> QuerySet:
+    def search_tasks(cls, query: str) -> QuerySet:
         query = Q(title__icontains=query) | Q(description__icontains=query)
         return cls.objects.filter(query)
+
 
 class Exercise(models.Model):
     name = models.CharField(max_length=100)
@@ -142,3 +143,23 @@ class Exercise(models.Model):
     difficulty_level = models.PositiveIntegerField()
     duration_minutes = models.PositiveIntegerField()
     repetitions = models.PositiveIntegerField()
+
+    @classmethod
+    def get_long_and_hard_exercises(cls) -> QuerySet:
+        query = Q(difficulty_level__gte=10) & Q(duration_minutes__gt=30)
+        return cls.objects.filter(query)
+
+    @classmethod
+    def get_short_and_easy_exercises(cls) -> QuerySet:
+        query = Q(difficulty_level__lte=5) & Q(duration_minutes__lt=15)
+        return cls.objects.filter(query)
+
+    @classmethod
+    def get_exercises_within_duration(cls,min_duration: int, max_duration: int)-> QuerySet:
+        query = Q(duration_minutes__gte=min_duration) & Q(duration_minutes__lte=max_duration)
+        return cls.objects.filter(query)
+
+    @classmethod
+    def get_exercises_with_difficulty_and_repetitions(cls,min_difficulty: int, min_repetitions: int) -> QuerySet:
+        query = Q(difficulty_level__gte=min_difficulty) & Q(repetitions__gte=min_repetitions)
+        return cls.objects.filter(query)
