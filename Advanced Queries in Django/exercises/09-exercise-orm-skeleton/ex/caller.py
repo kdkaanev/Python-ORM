50936af8d215154ee67ff776733900c8a271991a
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-11-22 06:07:58 +0200

    task4

--- Advanced Queries in Django/exercises/09-exercise-orm-skeleton/ex/caller.py
+++ Advanced Queries in Django/exercises/09-exercise-orm-skeleton/ex/caller.py
@@ -16,16 +16,4 @@ from main_app.models import RealEstateListing, VideoGame, Invoice, BillingInfo
 # Create BillingInfo instances with real addresses
 
 # Get invoices starting with a specific prefix
-invoices_with_prefix = Invoice.get_invoices_with_prefix("INV")
-for invoice in invoices_with_prefix:
-    print(f"Invoice Number with prefix INV: {invoice.invoice_number}")
 
-# Get invoices sorted by invoice number
-invoices_sorted = Invoice.get_invoices_sorted_by_number()
-for invoice in invoices_sorted:
-    print(f"Invoice Number: {invoice.invoice_number}")
-
-# Get an invoice by a specific invoice number along with its related billing info
-invoice = Invoice.get_invoice_with_billing_info("INV002")
-print(f"Invoice Number: {invoice.invoice_number}")
-print(f"Billing Info: {invoice.billing_info.address}")
