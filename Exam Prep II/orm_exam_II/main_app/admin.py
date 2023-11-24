26d56d3dca18932800e9b1b7cdf548c86f44e229
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-11-24 17:47:21 +0200

    task 2up

--- Exam Prep II/orm_exam_II/main_app/admin.py
+++ Exam Prep II/orm_exam_II/main_app/admin.py
@@ -6,18 +6,21 @@ from main_app.models import Profile, Product, Order
 # Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    fields = ['full_name', 'email', 'phone_number', 'is_active']
+    list_display = ['full_name', 'email', 'phone_number', 'is_active']
     search_fields = ['full_name ', 'email']
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    fields = ['name', 'price', 'in_stock', 'is_available']
+    list_display = ['name', 'price', 'in_stock', 'is_available']
     list_filter = ['is_available']
     search_fields = ['name ']
 
+
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    fields = ['profile', 'total_price', 'creation_date', 'is_completed']
+    list_display = ['profile', 'total_price', 'creation_date', 'is_completed']
     list_filter = ['is_completed']
     search_fields = ['profile__full_name ']
