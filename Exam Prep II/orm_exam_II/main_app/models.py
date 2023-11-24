4f2890d2e102c52890beca70c510e11381b96ad8
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-11-24 17:26:38 +0200

    exam2 task1

---
+++ Exam Prep II/orm_exam_II/main_app/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.core import validators
+
+
+class DateTime(models.Model):
+    class Meta:
+        abstract = True
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+
+# Create your models here.
+class Profile(DateTime):
+    full_name = models.CharField(
+        max_length=100,
+        validators=[validators.MinLengthValidator(2)]
+    )
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    address = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+
+class Product(DateTime):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[
+            validators.MinValueValidator(0.01)
+        ]
+    )
+    in_stock = models.PositiveIntegerField(
+        validators=[
+            validators.MinValueValidator(0)
+        ]
+    )
+    is_available = models.BooleanField(default=True)
+
+
+class Order (DateTime):
+    profile = models.ForeignKey(
+        to=Profile,
+        on_delete=models.CASCADE
+    )
+    products = models.ManyToManyField(to=Product)
+    total_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[
+            validators.MinValueValidator(0.01)
+        ]
+    )
+    is_completed = models.BooleanField(default=False)
